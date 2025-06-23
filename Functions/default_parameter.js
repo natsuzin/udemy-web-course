2192f9bf5a614ec52005caf030f5d6cb086ff2f4
Nathalia Suzin <natsuzin22@gmail.com>
2025-06-23 10:55:04 -0300

    update of fixes

--- Functions/default_parameter.js
+++ Functions/default_parameter.js
@@ -22,7 +22,7 @@ function sum2(a, b, c){
 console.log(sum2(), sum2(3), sum2(1, 2, 3), sum2(0, 0, 0));
 
 // es2015 version
-function sum2(a = 1, b = 1, c = 1){ // most suitable
+function sum3(a = 1, b = 1, c = 1){ // most suitable
     return a + b + c;
 }
 console.log(sum3(), sum3(3), sum3(1, 2, 3), sum3(0, 0, 0));

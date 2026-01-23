5979889de4bb9409ef19e93584a4ab327ec654b3
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2026-01-23 17:14:52 +0000

    object revision

---
+++ Object/revision.js
@@ -0,0 +1,36 @@
+// Dynamic collection of key/value pairs
+
+const product = new Object
+
+product.name = 'Chair'
+product['product brand'] = 'Generic'
+product.price = 220
+
+console.log(product)
+delete product.price
+delete product['product brand']
+console.log(product)
+
+/////
+
+const car = {
+    brand: 'Civic',
+    price: 85000,
+    owner: {
+        name: 'Edu',
+        age: 25,
+    },
+    conductors: [{
+        name: 'Nat',
+        age: 24
+    }],
+    calculateSafeValue: function() {
+        // ...
+    }
+}
+
+console.log(car)
+delete car.conductors
+delete car.calculateSafeValue
+console.log(car)
+console.log(car.conductors)

0e6d62da96865838105123e46b499a69bcaf3a39
Nathalia Suzin <natsuzin22@gmail.com>
2026-03-02 20:53:47 -0300

    array finished

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const sons = ['Robb', 'Brandon']
+const daughters = ['Sansa', 'Arya']
+const all = daughters.concat(sons)
+
+console.log(all, daughters, sons)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
--- Array/reduce.js
+++ Array/reduce.js
@@ -1,3 +1,5 @@
+// Reduce 
+
 const students = [
     { name: 'João', grade: 7.3, scholarship: false },
     { name: 'Maria', grade: 9.2, scholarship: true },
@@ -10,9 +12,35 @@ const result = students.map(a => a.grade).reduce(function(acc, grade) {
     return acc + grade
 })
 
-console.log(result) // 35.0, que é a soma de todas as notas dos alunos
+console.log(result) // 35.0, it's the sum of all the students' grades
 
 /////
 console.log('------------------')
 /////
 
+// Quest 1: are all the students on scholarships?
+
+const allScholarships = (result, scholarship) => result && scholarship
+console.log(students.map(a => a.scholarship).reduce(allScholarships)) // false
+
+// Quest 2: are there any scholarship students?
+
+const anyScholarship = (result, scholarship) => result || scholarship
+console.log(students.map(a => a.scholarship).reduce(anyScholarship))
+
+/////
+console.log('------------------')
+/////
+
+Array.prototype.reduce2 = function(callback, initialValue) {
+    const initialIndex = initialValue ? 0 : 1
+    let acumulator = initialValue || this [0]
+    for (let i=initialIndex; i < this.length; i++) {
+        acumulator = callback(acumulator, this[i], i, this)
+    }
+    return acumulator
+}
+
+const sum = (total, value) => total + value
+const nums = [1, 2, 3, 4, 5, 6]
+console.log(nums.reduce2(sum, 21))
